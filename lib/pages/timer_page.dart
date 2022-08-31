63e9aa1fa4179c65176c90791ef520aa22a59c05
default <jonathan.abraham@outlook.com>
2022-08-31 14:05:45 +0300

    feat: Initialize flutter app

---
+++ lib/pages/timer_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class TimerPage extends StatefulWidget {
+  const TimerPage({Key? key}) : super(key: key);
+
+  @override
+  State<TimerPage> createState() => _TimerPageState();
+}
+
+class _TimerPageState extends State<TimerPage> with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+      value: 100,
+
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+    ..addStatusListener((status) {
+      if (status == AnimationStatus.dismissed){
+        // Here add end of animation handlers
+        setState(() {
+        });
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Timer'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            children: <Widget>[
+
+              CircularProgressIndicator(
+                value: controller.value,
+                semanticsLabel: 'Circular progress indicator',
+
+              ),
+              Text(
+                (controller.value * (controller.duration?.inSeconds ?? 0.0)).toStringAsFixed(0),
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headline4,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: startTimer,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void startTimer() {
+    controller.value = 100;
+    controller.reverse();
+  }
+}
