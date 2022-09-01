60062666725ce8d59b3bc58dd02a3a48f947132c
default <jonathan.abraham@outlook.com>
2022-09-01 17:05:52 +0300

    feat: KISS way to make it works

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -14,14 +14,20 @@ class TimerScreen extends StatefulWidget {
 
 class _TimerScreenState extends State<TimerScreen>
     with TickerProviderStateMixin {
-  late AnimationController controller;
+  late AnimationController timerController;
+
+  int currentExerciseNb = 1;
+  int currentCycleNb = 1;
+
+  bool isExercise = true;
+
   Timer timer;
 
   _TimerScreenState(this.timer) {}
 
   @override
   void initState() {
-    controller = AnimationController(
+    timerController = AnimationController(
         vsync: this,
         duration: Duration(seconds: timer.exerciseTimeInSec),
         value: 100)
@@ -30,7 +36,7 @@ class _TimerScreenState extends State<TimerScreen>
       })
       ..addStatusListener((status) {
         if (status == AnimationStatus.dismissed) {
-          // Here add end of animation handlers
+          timerOver();
           setState(() {});
         }
       });
@@ -49,47 +55,89 @@ class _TimerScreenState extends State<TimerScreen>
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            children: <Widget>[
-              CircularProgressIndicator(
-                value: controller.value,
-                semanticsLabel: 'Circular progress indicator',
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Text('Cycle: ${currentCycleNb}'),
+                  Text('Exercice: ${currentExerciseNb}'),
+                ],
               ),
-              Text(
-                (controller.value * (controller.duration?.inSeconds ?? 0.0))
-                    .toStringAsFixed(0),
-                style: Theme.of(context).textTheme.headline4,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                children: <Widget>[
+                  CircularProgressIndicator(
+                    value: timerController.value,
+                    semanticsLabel: 'Circular progress indicator',
+                  ),
+                  Text(
+                    (timerController.value *
+                            (timerController.duration?.inSeconds ?? 0.0))
+                        .toStringAsFixed(0),
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
               ),
             ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: startTimer,
+        onPressed: () => startTimer(timer.exerciseTimeInSec),
         tooltip: 'Start',
         child: const Icon(Icons.play_arrow),
       ),
     );
   }
 
-  void startTimer() {
-    controller.value = 100;
-    controller.duration = Duration(seconds: timer.exerciseTimeInSec);
-    controller.reverse();
+  void startTimer(int duration) {
+    timerController.value = 100;
+    timerController.duration = Duration(seconds: duration);
+    timerController.reverse();
+  }
+
+  void timerOver() {
+    if (currentCycleNb <= timer.cycles) {
+      if (isExercise && currentExerciseNb < timer.exercisesNb) {
+        isExercise = false;
+        startTimer(timer.pauseBetweenExercises);
+        return;
+      }
+      if (isExercise && currentExerciseNb >= timer.exercisesNb) {
+        isExercise = false;
+        if (currentCycleNb < timer.cycles) {
+          startTimer(timer.pauseBetweenCycles);
+        }
+        return;
+      }
+      if (!isExercise && currentExerciseNb < timer.exercisesNb) {
+        isExercise = true;
+        currentExerciseNb++;
+        startTimer(timer.exerciseTimeInSec);
+        return;
+      }
+      if (!isExercise && currentExerciseNb >= timer.exercisesNb) {
+        currentCycleNb++;
+        if (currentCycleNb <= timer.cycles) {
+          isExercise = true;
+          currentExerciseNb = 1;
+          startTimer(timer.exerciseTimeInSec);
+        }
+      }
+    }
   }
 }
