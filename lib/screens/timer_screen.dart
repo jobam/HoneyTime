34a79d65651c41227a69983168cabae1e87a08a5
default <jonathan.abraham@outlook.com>
2022-09-07 22:14:33 +0200

    style: Timer with text

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -36,7 +36,7 @@ class _TimerScreenState extends State<TimerScreen>
       })
       ..addStatusListener((status) {
         if (status == AnimationStatus.dismissed) {
-          timerOver();
+          timerEndHandler();
           setState(() {});
         }
       });
@@ -45,6 +45,7 @@ class _TimerScreenState extends State<TimerScreen>
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -52,48 +53,58 @@ class _TimerScreenState extends State<TimerScreen>
         title: Text('Timer'),
       ),
       bottomNavigationBar: const MenuBottom(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Row(
-            children: [
-              Column(
+      body: Container(
+        height: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Flex(
+                direction: Axis.vertical,
                 children: [
-                  Text('Cycle: ${currentCycleNb}'),
-                  Text('Exercice: ${currentExerciseNb}'),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // Column is also a layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit its
-                // children horizontally, and tries to be as tall as its parent.
-                //
-                // Invoke "debug painting" (press "p" in the console, choose the
-                // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                // to see the wireframe for each widget.
-                //
-                // Column has various properties to control how it sizes itself and
-                // how it positions its children. Here we use mainAxisAlignment to
-                // center the children vertically; the main axis here is the vertical
-                // axis because Columns are vertical (the cross axis would be
-                // horizontal).
-                children: <Widget>[
-                  CircularProgressIndicator(
-                    value: timerController.value,
-                    semanticsLabel: 'Circular progress indicator',
-                  ),
-                  Text(
-                    (timerController.value *
-                            (timerController.duration?.inSeconds ?? 0.0))
-                        .toStringAsFixed(0),
-                    style: Theme.of(context).textTheme.headline4,
+                  SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 15,
+                        value: timerController.value,
+                        semanticsLabel: 'Circular progress indicator',
+                      )),
+                  Container(
+                    transform: Matrix4.translationValues(0.0, -138.0, 0.0),
+                    child: Text(
+                      (timerController.value *
+                              (timerController.duration?.inSeconds ?? 0.0))
+                          .toStringAsFixed(0),
+                      style: TextStyle(fontSize: 60, color: Colors.blue),
+                    ),
                   ),
                 ],
               ),
-            ],
-          ),
+            ]
+                // Padding(
+                //   padding: const EdgeInsets.all(20.0),
+                //   child: Center(
+                //     child: Row(
+                //       children: [
+                //         Column(
+                //           children: [
+                //             Text('Cycle: ${currentCycleNb}'),
+                //             Text('Exercice: ${currentExerciseNb}'),
+                //           ],
+                //         ),
+                //         Column(
+                //           mainAxisAlignment: MainAxisAlignment.center,
+                //           children: <Widget>[
+                //
+                //           ],
+                //         ),
+                //       ],
+                //     ),
+                //   ),
+                // ),
+                ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -110,7 +121,7 @@ class _TimerScreenState extends State<TimerScreen>
     timerController.reverse();
   }
 
-  void timerOver() {
+  void timerEndHandler() {
     if (currentCycleNb <= timer.cycles) {
       if (isExercise && currentExerciseNb < timer.exercisesNb) {
         isExercise = false;
